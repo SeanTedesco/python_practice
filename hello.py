672ebdcb0eb2060cbf3ca98fcfd600dfc4048c47
SeanTedesco <stedesco@live.com>
2020-02-23 02:40:18 -0500

    removed some code

--- hello.py
+++ hello.py
@@ -1,14 +1,6 @@
 import sys
 import requests
 
-# print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
 
 r = requests.get("https://www.facebook.com")
 print(r.status_code)
